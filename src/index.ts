ab4997f1c51d37de9773f5808b7ca14ed9c56f08
chichoon <chichoon.choi@gmail.com>
2022-10-16 18:43:09 +0900

    feat: socket.io 사용 테스트

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { initServer } from '@controllers/initServer';
 import http from 'http';
+import { Server } from 'socket.io';
 
 const PORT = process.env.PORT || 8080;
+const messageArr: string[] = [];
 
 function listenCallback() {
   console.log(`[${new Date().toLocaleTimeString()}] ${PORT} 에서 서버를 열었어요`);
@@ -10,6 +12,19 @@ function listenCallback() {
 async function openServer() {
   const app = initServer();
   const server = http.createServer(app);
+  const io = new Server(server, {
+    cors: {
+      origin: 'http://localhost:9000',
+      credentials: true,
+    },
+  });
+  io.on('connection', (socket) => {
+    socket.on('message', ({ name, message }) => {
+      console.log(message);
+      io.emit('messageList', messageArr);
+      messageArr.push(`[${name}]: ${message}`);
+    });
+  });
   server.listen(PORT, listenCallback);
 }
 
